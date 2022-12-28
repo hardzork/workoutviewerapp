a8ae45e38d763c0e4b12006a85391799193e5658
ROBINSON SANTOS ARAUJO DA SILVA JUNIOR <robinson.juniorr@gmail.com>
2022-12-28 13:33:16 -0300

    instalando o native-base

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 import {
   useFonts,
   Roboto_400Regular,
@@ -8,26 +9,13 @@ import {
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <NativeBaseProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? (
-        <Text style={{ fontFamily: "Roboto_700Bold" }}>
-          Open up App.tsx to start working on your app!
-        </Text>
-      ) : (
-        <View />
-      )}
-    </View>
+      {fontsLoaded ? <View /> : <View />}
+    </NativeBaseProvider>
   );
 }

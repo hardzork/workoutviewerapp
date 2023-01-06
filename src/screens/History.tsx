572ac08adfd8d5094e44fedf315d560ccc7ee778
ROBINSON SANTOS ARAUJO DA SILVA JUNIOR <robinson.juniorr@gmail.com>
2023-01-06 18:29:08 -0300

    mensagem de lista vazia

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,6 +1,6 @@
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { Heading, VStack, SectionList } from "native-base";
+import { Heading, VStack, SectionList, Text } from "native-base";
 import { useState } from "react";
 
 export function History() {
@@ -20,6 +20,15 @@ export function History() {
           <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
             {section.title}
           </Heading>
+        )}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: "center" }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda.{"\n"}
+            Vamos fazer exercícios hoje?
+          </Text>
         )}
       />
     </VStack>

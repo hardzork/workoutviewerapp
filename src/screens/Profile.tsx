27286166dd1c330ea834a363111aa5d09f51d1ea
ROBINSON SANTOS ARAUJO DA SILVA JUNIOR <robinson.juniorr@gmail.com>
2023-01-06 19:03:18 -0300

    botao alterar foto

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,44 @@
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, ScrollView, VStack } from "native-base";
+import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [isPhotoLoading, setIsPhotoLoading] = useState(true);
   return (
     <VStack>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: "https://github.com/hardzork.png" }}
-            alt="Robinson Junior"
-            size={33}
-          />
+          {isPhotoLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/hardzork.png" }}
+              alt="Robinson Junior"
+              size={PHOTO_SIZE}
+            />
+          )}
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
         </Center>
       </ScrollView>
     </VStack>

526424cdb8831edba29bbe11d8e338bbf11081cd
ROBINSON SANTOS ARAUJO DA SILVA JUNIOR <robinson.juniorr@gmail.com>
2023-01-08 17:48:21 -0300

    tipando os dados do formulário

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -7,15 +7,22 @@ import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { useForm, Controller } from "react-hook-form";
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 export function SignUp() {
-  const { control, handleSubmit } = useForm();
+  const { control, handleSubmit } = useForm<FormDataProps>();
   const navigation = useNavigation();
 
   function handleGoBack() {
     navigation.goBack();
   }
 
-  function handleSignUp(data: any) {
+  function handleSignUp(data: FormDataProps) {
     console.log(data);
   }
 

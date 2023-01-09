e46a5098d06aab3aae04d82d4cead935d2c1ce02
ROBINSON SANTOS ARAUJO DA SILVA JUNIOR <robinson.juniorr@gmail.com>
2023-01-09 19:10:48 -0300

    obtendo resposta do backend

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -53,7 +53,9 @@ export function SignUp() {
         email,
         password,
       }),
-    });
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   return (
